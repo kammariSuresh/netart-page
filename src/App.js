e32f4fd91d829eea55face106862a19fad7bff56
Suresh <kammarisuresh360@gmail.com>
2024-02-27 14:57:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <div className="main-img-container">
+          <img className="converted-n1k" src="https://res.cloudinary.com/djnaxt4mz/image/upload/v1709019397/ToBeConverted_n1kjop.png" alt="" />
+        </div>
+
+        <div className="main-card-container">
+            <img className="card-image" src="https://res.cloudinary.com/djnaxt4mz/image/upload/v1709016719/logo_n2s0jf.png" alt="heading" />
+            <h1>C.R.L PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th 
+              time.</h1>
+
+              <ul>
+
+                <li>C.R.I's energy efficient products are 
+              well recognized by various Government 
+              Institutions, as trustworthy products for
+              various projects across the 
+              globe to save energy.</li>
+              <li>C.R.I. is the highest contributor 
+              in the country for the projects
+              of EESL (Energy Efficiency Services Limited)
+                to replace the old inefficient pumps with 
+                5 Star rated energy efficient smart pumps
+                with IoT enabled control panel.</li>
+              </ul> 
+              
+            <img className="card-image-2" src="https://res.cloudinary.com/djnaxt4mz/image/upload/v1709016722/2_dxrdcp.png" alt="awards" />
+            <p>Government of India has awarded the 
+                "National Energy Conservation Award 2018".
+                 Mr. G. Selvaraj, Joint Managing Director 
+                 of C.R.I. Group received the award 
+                 from Smt. Sumitra Mahajan, Speaker of 
+                 Lok Sabha & Shri. Raj Kumar Singh, 
+                Honorable Minister of State.</p>
+        </div>
+      </div>
+
+      <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION</p>
+    
+    <img className="products-image" src="https://res.cloudinary.com/djnaxt4mz/image/upload/v1709016722/3_g3wtfa.png"  alt="products"/>
+    <p>Valves-pumps-Pipes-IoT Drives & Controllers-Wires & Cables-Solar Systems-Motors</p>
+
+    <hr/>
+    <div className="sec-container">
+      <h1>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h1>
+      <p>CHEMEALS & PROCESS | POWER|WATER & WASTE WATER OILS & GAS PHARMA SUGARS & DISTILLERIES PAPER & PULP MARINE & DEFENCE METAL & MINING FOOD & BEVERAGE PETROCHEMICAL & REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE & RESIDENTIAL</p>
+    </div>
+    <footer>
+          <ul className="footer-container">
+            <li className="item">
+            <img className='icon' src="https://res.cloudinary.com/djnaxt4mz/image/upload/v1708705519/assets/auto-group-pr1z_gyjaq2.png" alt="facebook" />
+
+              <a href="1800 200 1734" target="_blank">Toll Free 1800 200 1734</a>
+            </li>
+            <li className="item">
+              <img className='icon' src="https://res.cloudinary.com/djnaxt4mz/image/upload/v1708705519/assets/auto-group-thg3_an4ist.png" alt="facebook" />
+              <a href="www.facebook.com/cripumps" target="_blank" >www.facebook.com/cripumps</a>
+            </li>
+            <li className="item">
+            <img className='icon' src="https://res.cloudinary.com/djnaxt4mz/image/upload/v1708705519/assets/auto-group-anxp_hpwtn8.png" alt="facebook" />
+
+              <a href="www.cagroups.com" target="_blank">www.cagroups.com</a>
+            </li>
+          </ul>
+    </footer>
+    
     </div>
   );
 }
